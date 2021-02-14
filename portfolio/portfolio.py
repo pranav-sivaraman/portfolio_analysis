8e068a91e00f3dafcae25cd4652aff4ca3bdcaa6
Pranav Sivaraman <pranavsivaraman@gmail.com>
2021-02-13 22:52:19 -0500

    modified global variance function to handle variable amount of stocks

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -6,21 +6,23 @@ class Portfolio:
         self.balance = balance
         self.tickers = stocks
         self.stock_data = web.get_data_yahoo(self.tickers, start, end)
+        self.num_stocks = len(stocks)
         self.daily_returns = self.stock_data['Adj Close'].pct_change()
         self.mu = self.daily_returns.mean().to_numpy()[:,None]
         self.S = self.daily_returns.cov().to_numpy()
 
     def global_minimum_variance(self):
         np.set_printoptions(suppress=True)
-        M = np.block([[2 * self.S, np.ones((3,1))], [np.ones((1,3)), np.zeros(1)]])
-        soln = np.dot(np.linalg.inv(M), np.array([0, 0, 0, 1])[:,None])
-        xbar = soln[0:3]
+        M = np.block([[2 * self.S, np.ones((self.num_stocks,1))], [np.ones((1,self.num_stocks)), np.zeros(1)]])
+        soln = np.dot(np.linalg.inv(M), np.hstack([np.zeros(self.num_stocks), 1])[:,None])
+        
+        xbar = soln[0:self.num_stocks]
         risk = np.dot(np.dot(xbar.T, self.S), xbar)[0,0]
         ret = np.dot(xbar.T, self.mu)[0,0]
         portfolio = xbar * self.balance
 
         print(f"Risk: {risk}")
         print(f"Return: {ret}")
-        print(f"Portfolio: {portfolio}")
+        print(f"Portfolio: \n{portfolio}")
         
         
